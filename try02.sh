f1903a9237c539b9337145a3e65a785a4e721cad
dplocki <dawid.plocki@gmail.com>
2024-06-15 15:07:53 +0200

    feat: introduce the execute_command

--- try02.sh
+++ try02.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-dir="."
-cmd="ls -l $dir"
-output=$($cmd)
+function execute_command() {
+    local output="$($1)"
+    IFS=$'\n' read -r -d '' -a lines <<< "$output"
+}
 
-IFS=$'\n' read -r -d '' -a lines <<< "$output"
+dir="."
+cmd="git br"
+
+execute_command "$cmd"
 
 for line in "${lines[@]}"; do
     echo "Przetwarzanie: $line"
