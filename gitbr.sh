2df5fbc58bde4f067e1bf21c1d1f222e309a2644
dplocki <dawid.plocki@gmail.com>
2024-06-16 10:15:58 +0200

    refactor: format the gitbr.sh

--- gitbr.sh
+++ gitbr.sh
@@ -3,7 +3,7 @@
 choose_from_menu() {
     local outvar="$1"
     shift
-    local options=("$@") cur=0 count=${#options[@]} index=0
+    local options=("$@") cursor=0 count=${#options[@]} index=0
     local esc=$(echo -en "\e")
 
     while true
@@ -11,31 +11,34 @@ choose_from_menu() {
         index=0
         for option in "${options[@]}"
         do
-            if [ "$index" == "$cur" ]
-            then echo -e " >\e[7m$option\e[0m"
-            else echo "  $option"
+            if [ "$index" == "$cursor" ]; then
+                echo -e " >\e[7m$option\e[0m"
+            else
+                echo "  $option"
             fi
             index=$(( $index + 1 ))
         done
+
         read -s -n3 key
-        if [[ $key == $esc[A ]]
-        then cur=$(( $cur - 1 ))
-            [ "$cur" -lt 0 ] && cur=0
-        elif [[ $key == $esc[B ]]
-        then cur=$(( $cur + 1 ))
-            [ "$cur" -ge $count ] && cur=$(( $count - 1 ))
-        elif [[ $key == "" ]]
-        then break
+
+        if [[ $key == $esc[A ]]; then
+            cursor=$(( $cursor - 1 ))
+            [ "$cursor" -lt 0 ] && cursor=0
+        elif [[ $key == $esc[B ]]; then
+            cursor=$(( $cursor + 1 ))
+            [ "$cursor" -ge $count ] && cursor=$(( $count - 1 ))
+        elif [[ $key == "" ]]; then
+            break
         fi
+
         echo -en "\e[${count}A"
     done
 
-    printf -v $outvar "${options[$cur]}"
+    printf -v $outvar "${options[$cursor]}"
 }
 
 execute_command() {
-    local output="$($1)"
-    IFS=$'\n' read -r -d '' -a lines <<< "$output"
+    IFS=$'\n' read -r -d '' -a lines <<< "$($1)"
 }
 
 get_branch_from_line() {
