b7289f2a1749f3a27947378d8baf99a53ed2749f
dplocki <dawid.plocki@gmail.com>
2024-06-19 17:03:05 +0200

    feat: replace 'git br' with 'git branch'

--- git_switch_branch.sh
+++ git_switch_branch.sh
@@ -2,7 +2,7 @@
 
 source common.sh
 
-execute_command "git br --color=always"
+execute_command "git branch --color=always"
 choose_from_menu selected_choice "${lines[@]}"
 raw_branch_name=$(get_fist_column_ignore_start "$selected_choice")
 clean_branch_name=$(clean_color_markers "$raw_branch_name")
